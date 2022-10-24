9e12a23f159d93080943a6109d9837d984017b86
Kataku <kataku26@gmail.com>
2022-10-23 23:36:05 -0600

    Modelo Ho

--- Proyecto.m
+++ Proyecto.m
@@ -1,6 +1,9 @@
-clc; clear variables, clear figures; 
-% Integrantes: Daniel Alejandro Rodríguez Alvarado
-% Carné: C06575
+ % Integrantes: 
+%   Daniel Alejandro Rodríguez Alvarado C06575
+%   Nataly Delgado Huertas C02583
+%   Sylvia Fonseca Cruz C03039
+
+clc; clear variables, clear figures;
 
 %% Extracción de las mediciones
 M = readmatrix("delta_85a115.csv");
@@ -17,7 +20,7 @@ figure(2)
 plot(t, u, 'r', 'linewidth', 2)
 grid on
 
-%% Alfaro (123c)
+%% Modelo Alfaro (123c)
 % Se identifica el valor inicial y final de la entrada y respuesta
 yi = mean(yr(1:51)); % Media de 'yr' enre 0s y 1s
 yf = mean(yr(386:546)); % Media de 'yr' entre 4.5s y 5.5s
@@ -64,4 +67,39 @@ grid on
 
 % Índice integral de error absoluto
 e = abs(y.'-(yz+yi));
-JIAE = trapz(tz(3001:6001), e(3001:6001)); % Índice de error de 3s a 6s
+JIAE1 = trapz(tz(3001:6001), e(3001:6001)); % Índice de error de 3s a 6s
+
+%% Modelo Ho
+% Se halla p1 (35%)
+[~, ix35] = min(abs((yr(1:500)-yi) - 0.35*(yf-yi))); 
+[y35, t35] = deal(yr(ix35), t(ix35));
+
+% Se halla p2 (85%)
+[~, ix85] = min(abs((yr(1:500)-yi) - 0.85*(yf-yi))); 
+[y85, t85] = deal(yr(ix85), t(ix85));
+
+% Se hallan otros tiempos de interés 
+t3 = t35-t0;
+t4 = t85-t0;
+
+% Modelo 
+aHo = 0.670;
+bHo = 1.290;
+tauHo = aHo*(t4-t3);
+LHo = bHo*t3 + (1-b)*t4;
+P2 = K/(tauHo*s+1);
+tz = (0:0.001:10);
+
+% Se grafica la respuesta real con el modelo 123c de Alfaro
+figure(4)
+plot(tz, y, 'b', 'linewidth', 2)
+hold on
+[yz2, tz] = lsim(P2, entrada, tz);
+plot(tz, yz2+yi, 'r--', 'linewidth', 2)
+legend('Real', 'Ho')
+grid on
+
+% Índice integral de error absoluto
+e2 = abs(y.'-(yz2+yi));
+JIAE2 = trapz(tz(3001:6001), e2(3001:6001)); % Índice de error de 3s a 6s
+
