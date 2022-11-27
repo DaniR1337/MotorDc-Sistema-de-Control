5ce1606bfc88e14faf9fef9369a4a4d6ca89f020
SylFonseca97 <sylvia.fonseca@ucr.ac.cr>
2022-11-26 18:01:17 -0600

    Cambio en un comentario

--- IAE_Toolbox.m
+++ IAE_Toolbox.m
@@ -9,7 +9,7 @@ M = readmatrix("delta_85a115.csv");
 [t, u, yr] = deal(M(:, 1), M(:, 2), M(:, 3));
 
 % Se identifica el valor inicial y final de la entrada y respuesta
-yi = mean(yr(1:51)); % Media de 'yr' enre 0s y 1s
+yi = mean(yr(1:51)); % Media de 'yr' entre 0s y 1s
 yf = mean(yr(386:546)); % Media de 'yr' entre 4.5s y 5.5s
 ui = mean(u(1:51));
 uf = mean(u(386:546));
